2e7caf87fe555b84b267d6b1648c62d65688aac7
Phuzz24 <quangphu2424@gmail.com>
2024-11-07 12:27:33 +0700

    Add project files.

---
+++ KhaKhau/Controllers/CartController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KhaKhau.Controllers
+{
+    [Authorize]
+    public class CartController :Controller
+    {
+        private readonly ICartRepository _cartRepository;
+        public CartController(ICartRepository cartRepository ) 
+        {
+            _cartRepository = cartRepository;
+        }
+        public async Task<IActionResult> AddItem(int  productId, int qty = 1, int redirect=0)
+        {
+            var cartCount = await _cartRepository.AddItem(productId, qty);
+            if (redirect == 0)
+            {
+                return Ok(cartCount);
+            }
+            return RedirectToAction("GetUserCart");
+        } 
+        public async Task<IActionResult> RemoveItem(int  productId)
+        {
+           var cartCount=await _cartRepository.RemoveItem(productId);
+            return RedirectToAction("GetUserCart");
+        }public async Task<IActionResult> GetUserCart()
+        {
+           var cart=await _cartRepository.GetUserCart();
+            return View(cart);
+        }
+        
+        //public async Task<IActionResult> GetTotalItemInCart()
+        //{
+        //    int cartItem =_cartRepository.GetTo();
+        //    return View();
+        //}
+
+    }
+}
---
+++ KhaKhau/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using KhaKhau.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using KhaKhau.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Authorization;
+namespace KhaKhau.Controllers
+{
+    
+    public class ProductController : Controller
+    {
+        //user
+       
+
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        public ProductController(IProductRepository productRepository,
+        ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+        // Hiển thị danh sách sản phẩm
+        public async Task<IActionResult> Index(string sterm = "", int categoryId = 0)
+        {
+            // var products = await _productRepository.GetAllAsync();
+            var products = await _productRepository.GetProduct(sterm, categoryId);
+            //  var category = await _categoryRepository.GetAllAsync();
+            var category = await _productRepository.Categories();
+            ProDisplayModel proModel = new ProDisplayModel
+            {
+                Products = products,
+                Categories = category,
+                STerm = sterm,
+                CategoryId = categoryId
+            };
+
+            return View(proModel);
+        }
+
+        //sign in to dat ban
+        public ActionResult login()
+        {
+            return RedirectToPage("~/Identity/Account/Login");
+        }
+    }
+}
